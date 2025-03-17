4dfd8bb6bce9b439b4d0e04fbc63fd1403e2f4e4
Dillon Williams <dillonwilliams200161@gmail.com>
2025-03-17 23:55:20 +0530

    create save button

--- expense_tracker/lib/widgets/add_new_expense.dart
+++ expense_tracker/lib/widgets/add_new_expense.dart
@@ -23,69 +23,110 @@ class _AddNewExpenseState extends State<AddNewExpense> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        //add title text field
-        TextField(
-          controller: _titleController,
-          decoration: InputDecoration(
-            hintText: "Add new expense title",
-            label: Text("Title"),
+    return Padding(
+      padding: const EdgeInsets.all(15),
+      child: Column(
+        children: [
+          //add title text field
+          TextField(
+            controller: _titleController,
+            decoration: InputDecoration(
+              hintText: "Add new expense title",
+              label: Text("Title"),
+            ),
+            keyboardType: TextInputType.text,
+            maxLength: 50,
           ),
-          keyboardType: TextInputType.text,
-          maxLength: 50,
-        ),
-        //add amount text field
-        Row(
-          children: [
-            Expanded(
-              child: TextField(
-                controller: _amountController,
-                decoration: InputDecoration(
-                  hintText: "add amount",
-                  label: Text("Amount"),
+          //add amount text field
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  decoration: InputDecoration(
+                    hintText: "add amount",
+                    label: Text("Amount"),
+                  ),
+                  keyboardType: TextInputType.number,
                 ),
-                keyboardType: TextInputType.number,
               ),
-            ),
 
-            //date
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text("2025/12/12"),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.calendar_today),
-                  )
-                ],
+              //date
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text("2025/12/12"),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.calendar_today),
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
-        //add category dropdown
-        Row(
-          children: [
-            DropdownButton(
-              value: selectedCategory,
-              items: Category.values
-                  .map(
-                    (category) => DropdownMenuItem(
-                      value: category,
-                      child: Text(category.name),
-                    ),
-                  )
-                  .toList(),
-              onChanged: (value) {
-                setState(() {
-                  selectedCategory = value!;
-                });
-              },
-            )
-          ],
-        )
-      ],
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          //add category dropdown
+          Row(
+            children: [
+              DropdownButton(
+                value: selectedCategory,
+                items: Category.values
+                    .map(
+                      (category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(category.name),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  setState(
+                    () {
+                      selectedCategory = value!;
+                    },
+                  );
+                },
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(
+                      const Color.fromARGB(255, 242, 96, 83)),
+                ),
+                onPressed: () {},
+                child: Text(
+                  "Close",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(
+                      const Color.fromARGB(255, 49, 138, 221)),
+                ),
+                onPressed: () {},
+                child: Text(
+                  "Save",
+                  style: TextStyle(color: Colors.white),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
